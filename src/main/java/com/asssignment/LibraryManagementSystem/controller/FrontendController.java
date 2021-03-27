fa8bba81f3617b7145d133d2153201f65cbe6fb0
vikasmishra <vikas.mishra@posten.no>
2021-03-27 22:50:16 +0530

    Add support for rendering layout from backend

--- src/main/java/com/asssignment/LibraryManagementSystem/controller/FrontendController.java
+++ src/main/java/com/asssignment/LibraryManagementSystem/controller/FrontendController.java
@@ -1,17 +1,26 @@
 package com.asssignment.LibraryManagementSystem.controller;
 
+import com.asssignment.LibraryManagementSystem.service.FrontendService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
 
 @RestController
 public class FrontendController {
-    Logger LOG = LoggerFactory.getLogger(FrontendController.class);
+    private static final Logger LOG = LoggerFactory.getLogger(FrontendController.class);
+    private FrontendService frontendService;
 
-    @GetMapping("/")
-    public String appStatus() {
-        LOG.info("starting app");
-        return "App is up and running";
+    @Autowired
+    public FrontendController(FrontendService frontendService) {
+        this.frontendService = frontendService;
+    }
+
+    @GetMapping(value = {"/", ""}, produces = TEXT_HTML_VALUE)
+    public String fetchLayout() {
+        LOG.info("Loading layout for app");
+        return frontendService.fetchLayout();
     }
 }
